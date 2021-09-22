c2c33029def64600e6be5acae6ba5175f2ba649c
ivk101 <38563706+ivk101@users.noreply.github.com>
2021-09-22 20:10:00 +0500

    Add files via upload

---
+++ Icons/index.ts
@@ -0,0 +1,7 @@
+export * from './MenuIcon';
+export * from './ShareIcon';
+export * from './HideIcon';
+export * from './SaveIcon';
+export * from './CommentIcon';
+export * from './ComplaintIcon';
+export * from './IconAnon';
