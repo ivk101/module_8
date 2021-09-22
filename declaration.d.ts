5455bbe6ba74c28c88820ef78da6220a1949ce3c
ivk101 <38563706+ivk101@users.noreply.github.com>
2021-09-22 20:06:27 +0500

    Add files via upload

---
+++ declaration.d.ts
@@ -0,0 +1,5 @@
+declare module '*.png';
+declare module '*.jpg';
+declare module '*.svg' {  import React = require("react");
+  export const ReactComponent: React.SFC<React.SVGProps<SVGSVGElement>>;
+  const src: string;  export default src;}
